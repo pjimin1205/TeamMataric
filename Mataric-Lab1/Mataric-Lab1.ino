573c47c0c316d3f46b6c10f3fd8c509047a77b89
pjimin1205 <86455122+pjimin1205@users.noreply.github.com>
2025-12-02 10:32:38 -0500

    Update Mataric-Lab1.ino
    added basic stuff to lab1

--- Mataric-Lab1/Mataric-Lab1.ino
+++ Mataric-Lab1/Mataric-Lab1.ino
@@ -1,9 +1,169 @@
+//includew all necessary libraries
+#include <Arduino.h>//include for PlatformIO Ide
+#include <AccelStepper.h>//include the stepper motor library
+#include <MultiStepper.h>//include multiple stepper motor library
+
+//state LEDs connections
+#define redLED 5            //red LED for displaying states
+#define grnLED 6            //green LED for displaying states
+#define ylwLED 7            //yellow LED for displaying states
+#define enableLED 13        //stepper enabled LED
+int leds[3] = {5,6,7};      //array of LED pin numbers
+
+//define motor pin numbers
+#define stepperEnable 48    //stepper enable pin on stepStick 
+#define rtStepPin 50 //right stepper motor step pin 
+#define rtDirPin 51  // right stepper motor direction pin 
+#define ltStepPin 52 //left stepper motor step pin 
+#define ltDirPin 53  //left stepper motor direction pin 
+
+AccelStepper stepperRight(AccelStepper::DRIVER, rtStepPin, rtDirPin);//create instance of right stepper motor object (2 driver pins, low to high transition step pin 52, direction input pin 53 (high means forward)
+AccelStepper stepperLeft(AccelStepper::DRIVER, ltStepPin, ltDirPin);//create instance of left stepper motor object (2 driver pins, step pin 50, direction input pin 51)
+MultiStepper steppers;//create instance to control multiple steppers at the same time
+
+#define stepperEnTrue false //variable for enabling stepper motor
+#define stepperEnFalse true //variable for disabling stepper motor
+#define max_speed 1500 //maximum stepper motor speed
+#define max_accel 10000 //maximum motor acceleration
+
+int pauseTime = 2500;   //time before robot moves
+int stepTime = 500;     //delay time between high and low on step pin
+int wait_time = 1000;   //delay for printing data
+
+//define encoder pins
+#define LEFT 0        //left encoder
+#define RIGHT 1       //right encoder
+const int ltEncoder = 18;        //left encoder pin (Mega Interrupt pins 2,3 18,19,20,21)
+const int rtEncoder = 19;        //right encoder pin (Mega Interrupt pins 2,3 18,19,20,21)
+volatile long encoder[2] = {0, 0};  //interrupt variable to hold number of encoder counts (left, right)
+int lastSpeed[2] = {0, 0};          //variable to hold encoder speed (left, right)
+int accumTicks[2] = {0, 0};         //variable to hold accumulated ticks since last reset
+
+// Helper Functions
+
+//interrupt function to count left encoder tickes
+void LwheelSpeed() {
+  encoder[LEFT] ++;  //count the left wheel encoder interrupts
+}
+
+//interrupt function to count right encoder ticks
+void RwheelSpeed() {
+  encoder[RIGHT] ++; //count the right wheel encoder interrupts
+}
+
+void allOFF(){
+  for (int i = 0;i<3;i++){
+    digitalWrite(leds[i],LOW);
+  }
+}
+
+//function to set all stepper motor variables, outputs and LEDs
+void init_stepper(){
+  pinMode(rtStepPin, OUTPUT);//sets pin as output
+  pinMode(rtDirPin, OUTPUT);//sets pin as output
+  pinMode(ltStepPin, OUTPUT);//sets pin as output
+  pinMode(ltDirPin, OUTPUT);//sets pin as output
+  pinMode(stepperEnable, OUTPUT);//sets pin as output
+  digitalWrite(stepperEnable, stepperEnFalse);//turns off the stepper motor driver
+  pinMode(enableLED, OUTPUT);//set enable LED as output
+  digitalWrite(enableLED, LOW);//turn off enable LED
+  pinMode(redLED, OUTPUT);//set red LED as output
+  pinMode(grnLED, OUTPUT);//set green LED as output
+  pinMode(ylwLED, OUTPUT);//set yellow LED as output
+  digitalWrite(redLED, HIGH);//turn on red LED
+  digitalWrite(ylwLED, HIGH);//turn on yellow LED
+  digitalWrite(grnLED, HIGH);//turn on green LED
+  delay(pauseTime / 5); //wait 0.5 seconds
+  digitalWrite(redLED, LOW);//turn off red LED
+  digitalWrite(ylwLED, LOW);//turn off yellow LED
+  digitalWrite(grnLED, LOW);//turn off green LED
+
+  stepperRight.setMaxSpeed(max_speed);//set the maximum permitted speed limited by processor and clock speed, no greater than 4000 steps/sec on Arduino
+  stepperRight.setAcceleration(max_accel);//set desired acceleration in steps/s^2
+  stepperLeft.setMaxSpeed(max_speed);//set the maximum permitted speed limited by processor and clock speed, no greater than 4000 steps/sec on Arduino
+  stepperLeft.setAcceleration(max_accel);//set desired acceleration in steps/s^2
+  steppers.addStepper(stepperRight);//add right motor to MultiStepper
+  steppers.addStepper(stepperLeft);//add left motor to MultiStepper
+  digitalWrite(stepperEnable, stepperEnTrue);//turns on the stepper motor driver
+  digitalWrite(enableLED, HIGH);//turn on enable LED
+}
+
+//function prints encoder data to serial monitor
+void print_encoder_data() {
+  static unsigned long timer = 0;                           //print manager timer
+  if (millis() - timer > 100) {                             //print encoder data every 100 ms or so
+    lastSpeed[LEFT] = encoder[LEFT];                        //record the latest left speed value
+    lastSpeed[RIGHT] = encoder[RIGHT];                      //record the latest right speed value
+    accumTicks[LEFT] = accumTicks[LEFT] + encoder[LEFT];    //record accumulated left ticks
+    accumTicks[RIGHT] = accumTicks[RIGHT] + encoder[RIGHT]; //record accumulated right ticks
+    Serial.println("Encoder value:");
+    Serial.print("\tLeft:\t");
+    Serial.print(encoder[LEFT]);
+    Serial.print("\tRight:\t");
+    Serial.println(encoder[RIGHT]);
+    Serial.println("Accumulated Ticks: ");
+    Serial.print("\tLeft:\t");
+    Serial.print(accumTicks[LEFT]);
+    Serial.print("\tRight:\t");
+    Serial.println(accumTicks[RIGHT]);
+    encoder[LEFT] = 0;                          //clear the left encoder data buffer
+    encoder[RIGHT] = 0;                         //clear the right encoder data buffer
+    timer = millis();                           //record current time since program started
+  }
+}
+  
+/*function to run both wheels to a position at speed*/
+void runAtSpeedToPosition() {
+  stepperRight.runSpeedToPosition();
+  stepperLeft.runSpeedToPosition();
+}
+
+/*function to run both wheels continuously at a speed*/
+void runAtSpeed ( void ) {
+  while (stepperRight.runSpeed() || stepperLeft.runSpeed()) {
+  }
+}
+
+/*This function, runToStop(), will run the robot until the target is achieved and
+   then stop it
+*/
+void runToStop ( void ) {
+  int runNow = 1;
+  int rightStopped = 0;
+  int leftStopped = 0;
+
+  while (runNow) {
+    if (!stepperRight.run()) {
+      rightStopped = 1;
+      stepperRight.stop();//stop right motor
+    }
+    if (!stepperLeft.run()) {
+      leftStopped = 1;
+      stepperLeft.stop();//stop ledt motor
+    }
+    if (rightStopped && leftStopped) {
+      runNow = 0;
+    }
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  int baudrate = 9600; //serial monitor baud rate'
+  init_stepper(); //set up stepper motor
+
+  attachInterrupt(digitalPinToInterrupt(ltEncoder), LwheelSpeed, CHANGE);    //init the interrupt mode for the left encoder
+  attachInterrupt(digitalPinToInterrupt(rtEncoder), RwheelSpeed, CHANGE);   //init the interrupt mode for the right encoder
+
+
+  Serial.begin(baudrate);     //start serial monitor communication
+  Serial.println("Robot starting...Put ON TEST STAND");
+  delay(pauseTime); //always wait 2.5 seconds before the robot moves
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+//print_encoder_data();   //prints encoder data
 
+  delay(wait_time);               //wait to move robot or read data
 }
