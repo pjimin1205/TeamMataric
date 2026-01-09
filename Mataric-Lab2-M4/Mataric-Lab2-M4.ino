9a043ecdb7f42d9343cf68460669ad3f3f9c7c4f
pjimin1205 <86455122+pjimin1205@users.noreply.github.com>
2026-01-09 10:54:57 -0500

    simple dual core working

--- Mataric-Lab2-M4/Mataric-Lab2-M4.ino
+++ Mataric-Lab2-M4/Mataric-Lab2-M4.ino
@@ -1,8 +1,17 @@
 #include "Arduino.h"
 #include "RPC.h"
 
+//state LEDs connections
+#define redLED 5            //red LED for displaying states
+#define greenLED 6            //green LED for displaying states
+#define grnLED 6            //green LED for displaying states
+#define yellowLED 7            //yellow LED for displaying states
+#define ylwLED 7            //yellow LED for displaying states
+#define enableLED 13        //stepper enabled LED
+
 void setup() {
-  
+  pinMode(greenLED, OUTPUT);
+  digitalWrite(greenLED, HIGH);
 }
 
 void loop() {
--- Mataric-Lab2-M7/Mataric-Lab2-M7.ino
+++ Mataric-Lab2-M7/Mataric-Lab2-M7.ino
@@ -1,8 +1,19 @@
+#include "Arduino.h"
+#include "RPC.h"
+
+//state LEDs connections
+#define redLED 5            //red LED for displaying states
+#define greenLED 6            //green LED for displaying states
+#define grnLED 6            //green LED for displaying states
+#define yellowLED 7            //yellow LED for displaying states
+#define ylwLED 7            //yellow LED for displaying states
+#define enableLED 13        //stepper enabled LED
+
 void setup() {
   RPC.begin(); // boots M4
+  pinMode(redLED, OUTPUT);
+  digitalWrite(redLED, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
