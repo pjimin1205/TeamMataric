9e45513e3f967ead21f0603214a559ec54bb8a30
pjimin1205 <86455122+pjimin1205@users.noreply.github.com>
2026-01-15 20:32:20 -0500

    Add RPC communication for lidar and sonar data
    Introduced RPC server on M4 and client on M7 for reading lidar and sonar sensor data. Defined data structures and RPC bindings for sensor readings, and updated serial output to display sensor values.

--- Mataric-Lab2-M7/Mataric-Lab2-M7.ino
+++ Mataric-Lab2-M7/Mataric-Lab2-M7.ino
@@ -1,6 +1,9 @@
 #include "Arduino.h"
 #include "RPC.h"
 
+// main core
+// client
+
 //state LEDs connections
 #define redLED 5            //red LED for displaying states
 #define greenLED 6            //green LED for displaying states
@@ -11,9 +14,30 @@
 
 void setup() {
   RPC.begin(); // boots M4
-  pinMode(redLED, OUTPUT);
-  digitalWrite(redLED, HIGH);
+  Serial.begin(9600);
+  delay(100);
 }
 
 void loop() {
+  dist.front = read_lidar(frontLdr);
+  dist.back = read_lidar(backLdr);
+  dist.left = read_lidar(leftLdr);
+  dist.right = read_lidar(rightLdr);
+  dist2.right = read_sonar(rightSnr);
+  dist2.left = read_sonar(leftSnr);
+
+  RPC.println("front: "+ String(dist.front) +
+  ", back: "+ String(dist.back) + 
+  ", left: "+ String(dist.left) +
+  ", right: "+ String(dist.right));
+
+  RPC.println("front left: "+String(dist2.left) +
+  ", front right: "+ String(dist2.right));
+
+  struct lidar data = RPC.call("read_lidars").as<struct lidar>();
+  struct sonar data2 = RPC.call("read_sonars").as<struct sonar>();
+  RPC.println("front: "+ String(data.front) +
+  ", back: "+ String(data.back) +
+  ", left: "+ String(data.left) +
+  ", right: "+ String(data.right));
 }
